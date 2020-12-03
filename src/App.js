0b81e8b65b02eb8bdc9f2e9357004e11dd2918b9
Jakub Gawryl <jacqbus15@gmail.com>
2020-12-03 20:36:22 +0100

    TODO App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import {v4 as uuidv4} from 'uuid';
+import axios from 'axios';
+import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/todos/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/todos/AddTodo';
+import About from './components/pages/About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+class App extends Component {
+
+  state = {
+    todos: []
+  }
+
+  // Get data from remote 
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res =>  this.setState({todos: res.data})
+    )
+  }
+
+  // Toggle complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+
+        return todo;
+      })
+    })
+  }
+
+  // Add TODO
+  addTodo = title => {
+    if (!title) {
+      alert("You need to enter title!")
+      return;
+    }
+
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
+      id: uuidv4(),
+      title,
+      completed: false
+    }).then(res => {
+      this.setState({todos: [...this.state.todos, res.data]});
+    })
+
+  }
+
+  // Delete TODO
+  delTodo = (id) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res => {
+      console.log(res)
+      this.setState({todos: this.state.todos.filter(todo => todo.id !== id)})
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <div className="container">
+          <Header />
+
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo}/>
+              <Todos
+                todos={this.state.todos}
+                markComplete={this.markComplete}
+                delTodo={this.delTodo}
+              />
+            </React.Fragment>
+          )} />
+
+          <Route path="/about" component={About} />
+
+        </div>
+      </div>
+      </Router>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/todos/AddTodo.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+
+export class AddTodo extends Component {
+
+    state = {
+        title: ''
+    }
+
+    // [e.target.name] helps to set many state parameters. It based on name HTML property :)
+    onChange = e => this.setState({
+        [e.target.name]: e.target.value
+    })
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addTodo(this.state.title)
+        this.setState({
+            title: ''
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit} style={formStyle}>
+                <input
+                    style={inputStyle}
+                    type="text"
+                    name="title"
+                    placeholder="Add Todo..."
+                    value={this.state.title}
+                    onChange={this.onChange}
+                />
+
+                <input
+                    style={btnStyle}
+                    type="submit"
+                    value="Submit" 
+                    className="btn"
+                />
+            </form>
+        )
+    }
+}
+
+// Styles
+const formStyle = {
+    display: 'flex'
+};
+
+const inputStyle = {
+    flex: '10',
+    padding: '10px',
+    border: '1px solid #ddd'
+};
+
+const btnStyle = {
+    flex: '1'
+};
+
+export default AddTodo
